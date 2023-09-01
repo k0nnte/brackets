c406c185ae718573502c5f463be6b16c73edb026
k0nnte <bystrov.igor25@gmail.com>
2023-09-01 12:42:13 +0300

    complite

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const proverka = [];
+ 
+ 
+  for (const iterator of str) {
+    for(let i = 0; i<bracketsConfig.length;i++){
+   
+      if(iterator == bracketsConfig[i][0] && bracketsConfig[i][0] == bracketsConfig[i][1]){
+        if(proverka.includes(iterator)){
+              proverka.pop();
+              continue;
+            }else{
+              proverka.push(iterator);
+              continue;
+      }}
+      if(iterator == bracketsConfig[i][0]){
+        proverka.push(iterator);
+      }
+      if(iterator === bracketsConfig[i][1]){
+        if(proverka.pop() !== bracketsConfig[i][0]){
+          return false;
+        }
+        
+       
+      }
+    }
+    
+  }
+  if(proverka.length === 0){
+    return true;
+  }
+  return false;
 }
+
+
+
+
